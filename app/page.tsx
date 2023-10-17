b3cb1d5ea6bab5665c3e980920c08727ef73ae5e
Mohammed Awad <mawad@surconsultinggroup.com>
2023-10-17 19:32:33 +0000

    Removed unwanted changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,6 @@ import Dedicated from './components/Dedicated/index';
 import Digital from './components/Digital/index'; 
 import Ourteam from './components/Ourteam/index';
 import Featured from './components/Featured/index';
-import Articles from './components/Articles/index'
-import Beliefs from './components/Beliefs/index';
-import FAQ from './components/FAQ/index';
-import Insta from './components/Insta/index';
-import Join from './components/Joinus/index';
-import Manage from './components/Manage/index';
-import Testimonials from './components/Testimonials/index'
-import Wework from './components/Wework/index'
 
 
 export default function Home() {
@@ -23,14 +15,6 @@ export default function Home() {
       <Digital />
       <Ourteam />
       <Featured />
-      <Articles />
-      <Beliefs />
-      <FAQ />
-      <Insta />
-      <Join />
-      <Manage />
-      <Testimonials />
-      <Wework />
     </main>
   )
 }
