33e21684012e351ebcc759e0105d8e655106f548
maheshgupta563 <maheshguptaa563@gmail.com>
2023-10-17 20:25:19 +0530

    design-changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,14 @@ import Dedicated from './components/Dedicated/index';
 import Digital from './components/Digital/index'; 
 import Ourteam from './components/Ourteam/index';
 import Featured from './components/Featured/index';
+import Articles from './components/Articles/index'
+import Beliefs from './components/Beliefs/index';
+import FAQ from './components/FAQ/index';
+import Insta from './components/Insta/index';
+import Join from './components/Joinus/index';
+import Manage from './components/Manage/index';
+import Testimonials from './components/Testimonials/index'
+import Wework from './components/Wework/index'
 
 
 export default function Home() {
@@ -15,6 +23,14 @@ export default function Home() {
       <Digital />
       <Ourteam />
       <Featured />
+      <Articles />
+      <Beliefs />
+      <FAQ />
+      <Insta />
+      <Join />
+      <Manage />
+      <Testimonials />
+      <Wework />
     </main>
   )
 }
