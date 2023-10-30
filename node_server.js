ba8e6f0f237fcbdd6d5ee8dcb89e697d2a41b7a6
Mohammed Awad <mawad@surconsultinggroup.com>
2023-10-30 17:32:30 +0000

    Random update to node_server.js

---
+++ node_server.js
@@ -0,0 +1,9 @@
+// Node.js Server - 2025-02-14T15:46:38.269Z
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello from Node.js Server');
+});
+
+server.listen(3000, () => console.log('Server running on port 3000'));
